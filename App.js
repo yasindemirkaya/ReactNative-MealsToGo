fd2de755d0074060404ab8d35d2b66a1c2e458ee
yasindemirkaya <yasin97demirkaya@gmail.com>
2024-01-06 17:02:38 +0300

    Searchbar added

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+
+const isAndroid = Platform.OS === 'android'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>MealsToGo App</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar />
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto"></ExpoStatusBar>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: isAndroid ? StatusBar.currentHeight : 0
   },
+  search: {
+    padding: 16
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue'
+  }
 });
