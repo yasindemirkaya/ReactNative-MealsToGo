5784b54627ccfc6d24be70ec9f6e153452b6494f
yasindemirkaya <yasin97demirkaya@gmail.com>
2024-02-22 16:29:50 +0300

    commit
    commit

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 
+import { restaurantRequest } from "./src/services/restaurants/restaurants.service"
+
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
